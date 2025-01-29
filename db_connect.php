b5dee35b6e71cbef64804916bd6537f1af086a65
Christian06 <123466416+Hatdoglang@users.noreply.github.com>
2025-01-29 17:02:58 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,15 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','evaluation_db')or die("Could not connect to mysql".mysqli_error($con));
+$host = "b8occq7i8qfvinaczwwh-mysql.services.clever-cloud.com"; // Cloud Clever MySQL host
+$username = "uuovfe0ukxs2luyy"; // Replace with your Cloud Clever MySQL username
+$password = "sZXlCFtPlpIwOU0IzLK8"; // Replace with your Cloud Clever MySQL password
+$database = "b8occq7i8qfvinaczwwh"; // Cloud Clever database name
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully!";
+?>
