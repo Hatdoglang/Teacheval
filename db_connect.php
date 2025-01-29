139245e2a6ba01a14afb612de19c8fc9941b9dab
Christian06 <123466416+Hatdoglang@users.noreply.github.com>
2025-01-29 18:48:55 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,9 +5,14 @@ $password = getenv("DB_PASSWORD") ?: "sZXlCFtPlpIwOUOIzLK8";
 $dbname = getenv("DB_NAME") ?: "b8occq7i8qfvinaczwwh";
 $port = getenv("DB_PORT") ?: "3306";
 
-$conn = new mysqli($host, $user, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode = ''"
+    ]);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
